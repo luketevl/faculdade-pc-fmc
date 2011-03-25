7c8a823d78ae7bdc2827611afc95f0f716771525
luketevl <luketevl@9bea6405-944e-0903-1f54-6c9cb09f40a0>
2011-03-25 14:17:01 +0000

    Primeiro Versionamento

---
+++ trunk/Faculdade - PC_FMC/src/TESTE.java
@@ -0,0 +1,24 @@
+
+public class TESTE {
+	public static void main(String args[]){
+		/*int x;
+		for(x=0;x<20;x++){
+		System.out.println("Numero: "+(int)(999*Math.random()));
+		
+		}
+		System.out.println("oola");
+	*/
+		
+	double	it,lq,txDia,txMes,n,j,qo,sn;
+	
+	lq=Math.pow((1+1/100),(1/30)-1);
+	
+	txDia=lq*100;
+	//it=txMes/100;
+	
+		sn=Math.pow((1+1),1)*1;
+	
+	System.out.println(sn);
+	}
+
+}
