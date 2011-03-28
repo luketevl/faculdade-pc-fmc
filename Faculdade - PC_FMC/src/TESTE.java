ac53aa9d7cf2d23715c49671d988ef38d27b9f85
luketevl <luketevl@9bea6405-944e-0903-1f54-6c9cb09f40a0>
2011-03-28 03:15:58 +0000

    Implementado a validacao do Tipo da matriz A.

--- Faculdade - PC_FMC/src/TESTE.java
+++ Faculdade - PC_FMC/src/TESTE.java
@@ -1,13 +1,15 @@
-
+import java.util.Scanner;
 public class TESTE {
 	public static void main(String args[]){
+		Scanner op = new Scanner(System.in);
+		
 		/*int x;
 		for(x=0;x<20;x++){
 		System.out.println("Numero: "+(int)(999*Math.random()));
 		
 		}
 		System.out.println("oola");
-	*/
+	
 		
 	double	it,lq,txDia,txMes,n,j,qo,sn;
 	
@@ -18,7 +20,19 @@ public class TESTE {
 	
 		sn=Math.pow((1+1),1)*1;
 	
-	System.out.println(sn);
+	System.out.println(sn);*/
+		int x=2,y=2,n,i,j;
+		int matriz[][];
+		matriz=new int[x][y];
+		for (i = 0; i < x; i++) {
+			for (j = 0; j < y; j++) {
+				System.out.print("a"+(i+1)+""+(j+1)+":  ");
+				n = op.nextInt();
+				matriz[i][j] = n;
+			}
+		}
+		System.out.println(matriz[x-1][y-1] + " FDP " + matriz[x-1][0]);
+		
 	}
 
 }
